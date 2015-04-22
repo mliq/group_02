2b0f08f7bb50b693996c47f28345dbfa1558a185
mliq <liquori@gmail.com>
2015-04-22 16:26:26 -0500

    Added click functionality

--- script.js
+++ script.js
@@ -7,16 +7,21 @@ var Market = {
 	apples: randomNumber(50,999)/100,
 	oranges: randomNumber(50,999)/100,
 	bananas: randomNumber(50,999)/100,
-	grapes: randomNumber(50,999)/100,
 	pears: randomNumber(50,999)/100
 };
-
+var Person = {
+	apples: [],
+	oranges: [],
+	bananas: [],
+	pears: [],
+	budget: 100
+};
 function priceChange(price){
 	price += randomNumber(-50,50)/100;
-	if (price > 999){
-		price = 999;
-	}else if (price < 50){
-		price = 50;
+	if (price > 9.99){
+		price = 9.99;
+	}else if (price < .50){
+		price = .50;
 	}
 	return price;
 }
@@ -25,19 +30,25 @@ $(document).ready(function(){
 	$("#apples").append("<br>Price: $<p id='markAp'>" + Market.apples + "</p>");
 	$("#oranges").append("<br>Price: $<p id='markOr'>" + Market.oranges + "</p>");
 	$("#bananas").append("<br>Price: $<p id='markBa'>" + Market.bananas + "</p>");
-	$("#grapes").append("<br>Price: $<p id='markGr'>" + Market.grapes + "</p>");
 	$("#pears").append("<br>Price: $<p id='markPe'>" + Market.pears + "</p>");
+
 	setInterval(function(){
 		for (var fruit in Market){
-			console.log("work1");
-			fruit=priceChange(fruit);}
-			console.log("work2");
-		$("#markAp").html(Market.fruit);
-		$("#markOr").html(Market.fruit);
-		$("#markBa").html(Market.fruit);
-		$("#markGr").html(Market.fruit);
-		$("#markPe").html(Market.pears);
-		}, 1500);
+			Market[fruit]=priceChange(Market[fruit]);
+		}
 
+		$("#markAp").html(Market.apples);
+		$("#markOr").html(Market.oranges);
+		$("#markBa").html(Market.bananas);
+		$("#markPe").html(Market.pears);
+	}, 1500);
+
+		$(".btn").on("click",function (){
+
+		Person[this.id].push(Market[this.id]);
+		Person.budget -= Market[this.id];
+			
+
+		});
 });
 
